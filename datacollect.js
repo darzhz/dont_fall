0885a8aac0e0f58f6d7a52174ec26f0fac618eb2
Darsh shyam kumar <68496521+darzhz@users.noreply.github.com>
2022-02-22 23:02:55 +0530

    well is a slower way
    so I was thinking about collecting training data and saving it however this might be very slow and cumbersome

--- datacollect.js
+++ datacollect.js
@@ -1,12 +1,62 @@
+let img;
+let poses = [];
+let poseNet;
+let pose;
+let structure;
+let imageState = 'loading';
+let targetLabel = ['standing']
+let i;
 function setup(){
-  
+createCanvas(windowWidth,windowHeight);
+background(155);
+i=0;
+let data = setInterval(()=>{
+  loadimg(`${i}.jpg`);
+  console.log(i);
+  i++
+  if(i>2){
+    clearInterval(data);
+  }
+},7000)
 }
-function draw(){
-  
+function drawer(){
+  background(0);
+  translate(0,0);
+  image(img, 0, 0);
+  if(poses.length >0){
+     pose = poses[0].pose;
+     console.log(pose.nose)
+    structure = poses[0].skeleton;
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(255, 0, 0);
+      ellipse(x, y, 16, 16);
+    }
+    imageState = 'loading';
+  }
+  //noLoop();
 }
-function loadimg(filename){
-  
+async function loadimg(filename){
+  img = await createImg(`${filename}`,imageReady);
+  img.size(width,height);
+  console.log("resolution = "+img.width+"x"+img.height);
+  img.hide();
 }
-function getPose(){
-  
+async function imageReady(){
+  const options = {
+    minConfidence: 0.1,
+    inputResolution: { width, height },
+  };
+  console.log('image has been loaded');
+  poseNet = new ml5.poseNet(getPose,options);
+ await poseNet.on("pose",(results)=>{
+    poses = results;
+    pose = poses[0].pose;
+    imageState = 'ready';
+    drawer();
+  });
+}
+async function getPose(){
+  await poseNet.singlePose(img);
 }
